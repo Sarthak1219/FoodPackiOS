1666c46547e2ab161cbf1869b0cca8a85d3ea28b
smehta9 <smehta9@ncsu.edu>
2020-09-02 22:30:28 -0400

    [Testing]
    Fully Tested Restaurant Object.

--- FoodPackiOSTests/RestaurantTests.swift
+++ FoodPackiOSTests/RestaurantTests.swift
@@ -23,7 +23,6 @@ class RestaurantTests: XCTestCase {
     let test_pickup_time: String = "2020-09-02 14:48:03";
     let test_inventory_message: String = "Copy Inventory Here";
     let test_volunteer_message: String = "Thanks for helping reduce food waste in our community!";
-    var test_is_ready: Int = 0;
     
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -37,7 +36,8 @@ class RestaurantTests: XCTestCase {
      Tests the Restaurant's Constructor; Testing for use in RestaurantInputTests class.
      */
     func testRestaurantConstructor() {
-        var r = Restaurant(restaurant_ID: test_restaurant_ID, restaurant_name: test_restaurant_name, restaurant_address: test_restaurant_address, latitude: test_latitude, longitude: test_longitude, pickup_time: test_pickup_time, inventory_message: test_inventory_message, volunteer_message: test_volunteer_message, is_ready: test_is_ready);
+        //r is let, because is_ready will not be changed
+        let r = Restaurant(restaurant_ID: test_restaurant_ID, restaurant_name: test_restaurant_name, restaurant_address: test_restaurant_address, latitude: test_latitude, longitude: test_longitude, pickup_time: test_pickup_time, inventory_message: test_inventory_message, volunteer_message: test_volunteer_message, is_ready: 0);
         
         XCTAssertEqual(r.restaurant_ID, test_restaurant_ID);
         XCTAssertEqual(r.restaurant_name, test_restaurant_name);
@@ -48,14 +48,47 @@ class RestaurantTests: XCTestCase {
         XCTAssertEqual(r.inventory_message, test_inventory_message);
         XCTAssertEqual(r.volunteer_message, test_volunteer_message);
         //getter necessary, bc is_ready is private
-        XCTAssertEqual(r.getIsReady(), test_is_ready);
+        XCTAssertEqual(r.getIsReady(), 0);
+    }
+    
+    /**
+     Tests Restaurant's implementation of Equatable Protocol's method ==; Testing for use in RestaurantInputTests class.
+     */
+    func testequatablemethod(){
+        //r1 is let, because is_ready will not be changed
+        let r1 = Restaurant(restaurant_ID: test_restaurant_ID, restaurant_name: test_restaurant_name, restaurant_address: test_restaurant_address, latitude: test_latitude, longitude: test_longitude, pickup_time: test_pickup_time, inventory_message: test_inventory_message, volunteer_message: test_volunteer_message, is_ready: 0);
+        //r2 is let, because is_ready will not be changed
+        let r2 = Restaurant(restaurant_ID: test_restaurant_ID, restaurant_name: test_restaurant_name, restaurant_address: test_restaurant_address, latitude: test_latitude, longitude: test_longitude, pickup_time: test_pickup_time, inventory_message: test_inventory_message, volunteer_message: test_volunteer_message, is_ready: 0);
+        //r3 is let, because is_ready will not be changed
+        let r3 = Restaurant(restaurant_ID: test_restaurant_ID, restaurant_name: test_restaurant_name, restaurant_address: test_restaurant_address, latitude: test_latitude, longitude: test_longitude, pickup_time: test_pickup_time, inventory_message: test_inventory_message, volunteer_message: test_volunteer_message, is_ready: 1);
+        
+        XCTAssertTrue(r1 == r2);
+        XCTAssertFalse(r1 == r3);
+    }
+    
+    /**
+     Tests Restaurant's turnOffIsReady Method.
+     */
+    func testTurnOffIsReady() {
+        //r1 is var, because is_ready may be changed
+        var r1 = Restaurant(restaurant_ID: test_restaurant_ID, restaurant_name: test_restaurant_name, restaurant_address: test_restaurant_address, latitude: test_latitude, longitude: test_longitude, pickup_time: test_pickup_time, inventory_message: test_inventory_message, volunteer_message: test_volunteer_message, is_ready: 1);
+        
+        r1.turnOffIsReady();
+        XCTAssertEqual(r1.getIsReady(), 0);
+        r1.turnOffIsReady();
+        XCTAssertEqual(r1.getIsReady(), 0);
+        
+        var r2 = Restaurant(restaurant_ID: test_restaurant_ID, restaurant_name: test_restaurant_name, restaurant_address: test_restaurant_address, latitude: test_latitude, longitude: test_longitude, pickup_time: test_pickup_time, inventory_message: test_inventory_message, volunteer_message: test_volunteer_message, is_ready: 0);
+        
+        r2.turnOffIsReady();
+        XCTAssertEqual(r2.getIsReady(), 0);
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
